68f60695e326e7db64b9b9c558295c02d3fdb587
Alexandr <alexandr8107ivanov@gmail.com>
2024-02-03 19:40:22 +0100

    initial commit

---
+++ pole.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class pole {
+    public static void main(String[] args) {
+       
+        String[] myArray = new String[5];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Zadejte hodnotu prvku " + (i + 1) + ": ");
+            myArray[i] = scanner.nextLine();
+        }
+        
+        System.out.println("Seznam vyplnenych poli:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Zivel " + (i + 1) + ": " + myArray[i]);
+        }
+        scanner.close();
+    }
+}
